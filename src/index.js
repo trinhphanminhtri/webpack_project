7e8dc7c9e93e1c66617ca4565d18488e37dd575c
Trinh Phan Minh Tri <trinhphanminhtri@gmail.com>
2023-10-09 16:52:03 +0700

    Don dep thu muc build mot cach tu dong

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import sum from './utils';
 import './styles/style.css';
 import './styles/style.scss';
 
-console.log(sum(2, 3));
+console.log(sum(100, 40));
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
+    clean: true, // dọn dẹp thư mục sau khi build
   },
 
   module: {
