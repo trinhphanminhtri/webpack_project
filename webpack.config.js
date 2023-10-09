7c5b75633fd0f444a6ff8dcbcb81ba75562db1fb
Trinh Phan Minh Tri <trinhphanminhtri@gmail.com>
2023-10-09 17:29:05 +0700

    Sourcemap cho webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,52 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    app: path.resolve('src/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    clean: true, // dọn dẹp thư mục sau khi build
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss|css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Webpack App',
-      filename: 'index.html',
-      template: 'src/template.html',
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: 'dist', // đường dẫn tương đối đến với thư mục chứa index.html
+module.exports = (env) => {
+  const isDevelopment = Boolean(env.development);
+  // console.log(env.development);
+  // console.log(env.production);
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    entry: {
+      app: path.resolve('src/index.js'),
     },
-    port: 3000, // port thay cho port mặc định (8080)
-    open: {
-      app: {
-        name: 'chrome',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      clean: true, // dọn dẹp thư mục sau khi build
+    },
+    devtool: isDevelopment ? 'source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss|css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Webpack App',
+        filename: 'index.html',
+        template: 'src/template.html',
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: 'dist', // đường dẫn tương đối đến với thư mục chứa index.html
       },
-    }, // mở trang web khi chạy webpack serve
-    hot: true, // bật tính năng reload nhanh Hot Module Replacement
-    compress: true, // bất Gzip cho các tài nguyên
-    historyApiFallback: true, // set true nếu dùng cho SPA và sử dụng History API của HTML5, ví dụ sử dụng React router không bị lỗi
-  },
+      port: 3000, // port thay cho port mặc định (8080)
+      open: {
+        app: {
+          name: 'chrome',
+        },
+      }, // mở trang web khi chạy webpack serve
+      hot: true, // bật tính năng reload nhanh Hot Module Replacement
+      compress: true, // bất Gzip cho các tài nguyên
+      historyApiFallback: true, // set true nếu dùng cho SPA và sử dụng History API của HTML5, ví dụ sử dụng React router không bị lỗi
+    },
+  };
 };
