3347fda06e876d7ae5a1ed493ea08d6ff7a70239
Trinh Phan Minh Tri <trinhphanminhtri@gmail.com>
2023-10-09 16:48:08 +0700

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -30,4 +30,18 @@ module.exports = {
       template: 'src/template.html',
     }),
   ],
+  devServer: {
+    static: {
+      directory: 'dist', // đường dẫn tương đối đến với thư mục chứa index.html
+    },
+    port: 3000, // port thay cho port mặc định (8080)
+    open: {
+      app: {
+        name: 'chrome',
+      },
+    }, // mở trang web khi chạy webpack serve
+    hot: true, // bật tính năng reload nhanh Hot Module Replacement
+    compress: true, // bất Gzip cho các tài nguyên
+    historyApiFallback: true, // set true nếu dùng cho SPA và sử dụng History API của HTML5, ví dụ sử dụng React router không bị lỗi
+  },
 };
